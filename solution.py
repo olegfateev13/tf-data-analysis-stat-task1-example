96ef27eec5ad00671f831e848651f10b62cae84d
olegfateev13 <58751132+olegfateev13@users.noreply.github.com>
2023-04-04 17:11:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
+from scipy.stats import lognorm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
+chat_id = 263008738 # Ваш chat ID, не меняйте название переменной
+def log_likelihood(a, x):
+    return -np.sum(lognorm.logpdf(x, s=a))
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    result = minimize(log_likelihood, 1, args=(x,))
+    return result.x[0] # Ваш ответ
